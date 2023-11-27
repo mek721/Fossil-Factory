07bad86569a8bf539f8431182d68868ab8c4c426
MaxCMartinsen <max.castoe@outlook.com>
2023-11-27 21:45:43 +0100

    gg

--- Fossil-Factory/Assets/Scripts/GridManager.cs
+++ Fossil-Factory/Assets/Scripts/GridManager.cs
@@ -21,7 +21,7 @@ public class GridManager : MonoBehaviour
         {
             for (int y = 0; y < _height; y++)
             {
-                var spawnedTile = Instantiate(_tilePrefab,new Vector3(x, y), Quaternion.identity);
+                var spawnedTile = Instantiate(_tilePrefab,new Vector3(x, y), Quaternion.identity, transform);
                 spawnedTile.name = $"Tile {x} (y)";
 
                 var isOffset = (x % 2 == 0 && y % 2 != 0 || (x % 2 != 0 && y % 2 == 0));
