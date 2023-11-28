d17af582feb7c2c51de8331daff1d51b6832bed0
mek721 <152213984+mek721@users.noreply.github.com>
2023-11-28 18:18:42 +0000

    zoom

--- Fossil-Factory/Assets/Scripts/CameraScript.cs
+++ Fossil-Factory/Assets/Scripts/CameraScript.cs
@@ -10,9 +10,14 @@ public class CameraScript : MonoBehaviour
     public float minY = -5f;
     public float maxY = 5f;
 
+    public float zoomSpeed = 5f; // New variable for zoom speed
+    public float minZoom = 5f;  // Minimum zoom value
+    public float maxZoom = 15f; // Maximum zoom value
+
     void Update()
     {
         MoveCamera();
+        ZoomCamera();
     }
 
     void MoveCamera()
@@ -27,5 +32,15 @@ public class CameraScript : MonoBehaviour
         float clampedX = Mathf.Clamp(transform.position.x, minX, maxX);
         float clampedY = Mathf.Clamp(transform.position.y, minY, maxY);
         transform.position = new Vector3(clampedX, clampedY, transform.position.z);
+    }
+
+    void ZoomCamera()
+    {
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+
+        // Adjust the size of the camera based on the mouse wheel input
+        float newSize = Camera.main.orthographicSize - scrollInput * zoomSpeed * Time.deltaTime;
+        newSize = Mathf.Clamp(newSize, minZoom, maxZoom);
+        Camera.main.orthographicSize = newSize;
     }
 }
