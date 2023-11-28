0f3fa8e170284d2aaba59a6aa68b9d467970cce3
MaxCMartinsen <max.castoe@outlook.com>
2023-11-28 20:20:41 +0100

    Printer

---
+++ Fossil-Factory/Assets/Fossils/Fossil.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Fossil", menuName = "Fossil")]
+public class Fossil : ScriptableObject
+{
+    public float realness;
+    public float sellPrice;
+    public Sprite sprite;
+}
--- Fossil-Factory/Assets/Scripts/Printer.cs
+++ Fossil-Factory/Assets/Scripts/Printer.cs
@@ -2,7 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Printer : MonoBehaviour
+[CreateAssetMenu(fileName = "Printer", menuName = "Printer")]
+public class Printer : ScriptableObject
 {
-    
+    public float _productionRate;
+    public float _cost;
+    public float _price;
+    public Sprite sprite;
 }
